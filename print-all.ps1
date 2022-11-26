e92c93ae8dd1ce8b8e1190933ad7f30643ad1cd2
Seiichi Ariga <seiichi.ariga@gmail.com>
2022-11-26 18:04:48 +0900

    印刷スクリプト

--- print-all.ps1
+++ print-all.ps1
@@ -1,1 +1,17 @@
-Start-Process -filepath output\*.xlsx -Verb print
+# Seiichi Ariga <seiichi.ariga@gmail.com>
+# .\output 内のExcelファイルを印刷する
+# Windowsのみ対応
+
+$currentDir = Get-Location
+$workDir = $currentDir.Path + "\output\"
+
+# Get-ChildItemはオブジェクトを返すので、ForEach-Objectが必要っぽい
+$fileList = Get-ChildItem $workdir -Filter *.xlsx | ForEach-Object { "$_" }
+
+Write-Output "以下のファイルを印刷します"
+Write-Output $fileList
+
+# 各ファイルに対してExcelを起動して印刷(Windowsのみ)
+ForEach($file in $fileList) {
+    Start-Process -FilePath $file -Verb print
+}
